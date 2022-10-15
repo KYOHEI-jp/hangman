e40af2720e7e53df24c25ac3ed37be25776f3df7
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-10-15 11:52:09 +0900

    utils

---
+++ lib/utils.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+TextStyle retroStyle(double size, [Color? color, FontWeight? fw]) {
+  return TextStyle(
+    fontFamily: "RetroGaming",
+    color: color,
+    fontSize: size,
+    fontWeight: fw,
+  );
+}
