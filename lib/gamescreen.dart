c9fa7cef2fc2dfbb46634f0fd46fc5fe0b9c8c07
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-12-04 11:45:17 +0900

    InkWell追加、角丸

--- lib/gamescreen.dart
+++ lib/gamescreen.dart
@@ -14,6 +14,7 @@ class _GameScreenState extends State<GameScreen> {
     return Scaffold(
       backgroundColor: Colors.black45,
       appBar: AppBar(
+        elevation: 10,
         backgroundColor: Colors.black45,
         centerTitle: true,
         title: Text(
@@ -28,6 +29,80 @@ class _GameScreenState extends State<GameScreen> {
             icon: Icon(Icons.volume_up_sharp),
           )
         ],
+      ),
+      body: SingleChildScrollView(
+        physics: ScrollPhysics(),
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 20.0),
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width / 3.5,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlueAccent,
+                ),
+                height: 30,
+                child: Center(
+                  child: Text(
+                    "12 points",
+                    style: retroStyle(15, Colors.white, FontWeight.w700),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Image(
+                width: 155,
+                height: 155,
+                fit: BoxFit.cover,
+                color: Colors.white,
+                image: AssetImage(
+                  "images/hangman0.png",
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Text(
+                "7 lives left",
+                style: retroStyle(18, Colors.grey, FontWeight.w700),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Text(
+                "??????",
+                style: retroStyle(35, Colors.white, FontWeight.w700),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              GridView.count(
+                crossAxisCount: 7,
+                shrinkWrap: true,
+                padding: EdgeInsets.only(left: 10),
+                childAspectRatio: 1.3,
+                physics: NeverScrollableScrollPhysics(),
+                children: letters.map((alphabet) {
+                  return InkWell(
+                    borderRadius: BorderRadius.circular(10),
+                    onTap: () => print("Tapped"),
+                    child: Center(
+                      child: Text(
+                        alphabet,
+                        style: retroStyle(20, Colors.white, FontWeight.w700),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
